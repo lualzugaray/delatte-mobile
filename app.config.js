6b063c094131eee0f52894559e6b94a76d92de1b
Lucia Alzugaray <lucia.alzugaray24@gmail.com>
2025-07-17 06:57:19 -0300

    add: app config

---
+++ app.config.js
@@ -0,0 +1,15 @@
+export default {
+    expo: {
+      name: 'delatte-mobile',
+      slug: 'delatte-mobile',
+      extra: {
+        EXPO_PUBLIC_API_URL: 'https://delatte-api.onrender.com/api',
+        EXPO_PUBLIC_AUTH0_DOMAIN: 'dev-d82ap42lb6n7381y.us.auth0.com',
+        EXPO_PUBLIC_AUTH0_CLIENT_ID: '6KjPP78Z2wqYyLiKGVHdwgttjiXuULLK',
+        EXPO_PUBLIC_AUTH0_BACKEND_CLIENT_ID: 'UfkvGwQ7iTGiBtQ0jBsD6rps5n6FivJ8',
+        EXPO_PUBLIC_AUTH0_BACKEND_CLIENT_SECRET: 'XXXXXXXXX',
+        EXPO_PUBLIC_AUTH0_AUDIENCE: 'https://delatte.api',
+      }
+    }
+  }
+  
